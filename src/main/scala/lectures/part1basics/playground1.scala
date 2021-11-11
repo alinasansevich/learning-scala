f47ea0bbed43f3108fbbf5921ea17f395e3dece5
Alina Sansevich <alinasansevich@Alinas-Air.arriswifi.net>
2021-11-11 12:58:12 -0600

    First commit on Mac.

---
+++ src/main/scala/lectures/part1basics/playground1.scala
@@ -0,0 +1,11 @@
+package lectures.part1basics
+
+object playground1 extends App {
+
+  // write 3 simple functions - use default args, use call by name/value
+
+  // write some recursive functions from Reddit's comments
+
+  // solve ROSALIND's problems with Scala - String formatting
+
+}
